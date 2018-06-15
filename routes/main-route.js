a1eea38e43e69698f30d8cda6c7c8031bd7940b9
AnthonyMarc23 <anthony@adaptivelite.com>
2018-06-15 10:22:12 -0400

    Added routes

---
+++ routes/main-route.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const express = require("express");
+const mainRouter = express.Router();
+
+mainRouter.get("/", (req,res) => {
+
+});
+
+mainRouter.post("/", (req,res) => {
+
+});
+
+mainRouter.put("/", (req,res) => {
+
+});
+
+mainRouter.delete("/", (req,res) => {
+
+});
+
+module.exports = mainRouter;
