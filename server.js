9b39d37c8393f1114c32345965ffb09acbb86c69
AnthonyMarc23 <anthony@adaptivelite.com>
2018-06-19 14:01:22 -0400

    Heroku-related files

--- server.js
+++ server.js
@@ -12,3 +12,6 @@ app.use(express.static(__dirname + "/app"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
+
+
+// postgres://lcehcyetvnqzhm:daaef64f38e9695607e0420b0f0628b49b41b5918946525d9d397e0555fa3e1e@ec2-50-19-86-139.compute-1.amazonaws.com:5432/dfge3la2d6fqkn
