3037f301ba9ae3fb347fd78c017e6a8c97f36bae
AnthonyMarc23 <anthony@adaptivelite.com>
2018-06-26 09:17:37 -0400

    route changes

--- routes/main-route.js
+++ routes/main-route.js
@@ -9,12 +9,7 @@ const pool = require("../pg-connection-pool");
 mainRouter.get("/storedata", (request, response) => {
     pool.query("SELECT * FROM fooddatabase ORDER BY id").then((result) => {
       response.send(result.rows);
-<<<<<<< HEAD
       pool.end();
-=======
-      // will done() work?
-      // done();
->>>>>>> 6bf8f63f3856f20a76ae1f4cd35cb283a299659b
     });
 });
 
@@ -23,12 +18,7 @@ mainRouter.post("/storedata", (request, response) => {
   pool.query("INSERT INTO fooddatabase(food_name, cal, img, protein, fat, carbs, serving_qty, serving_unit, serving_weight_grams) VALUES($1::text, $2::float, $3::text, $4::float, $5::float, $6::float, $7::int, $8::text, $9::float)", [request.body.data.foods[0].food_name, request.body.data.foods[0].nf_calories, request.body.data.foods[0].photo.highres, request.body.data.foods[0].nf_protein, request.body.data.foods[0].nf_total_fat, request.body.data.foods[0].nf_total_carbohydrate, request.body.data.foods[0].serving_qty, request.body.data.foods[0].serving_unit, request.body.data.foods[0].serving_weight_grams]).then(() => {
     pool.query("SELECT * FROM fooddatabase ORDER BY id").then((result) => {
       response.send(result.rows);
-<<<<<<< HEAD
       pool.end();
-=======
-      // will done() work?
-      // done();
->>>>>>> 6bf8f63f3856f20a76ae1f4cd35cb283a299659b
     });
   });
 });
