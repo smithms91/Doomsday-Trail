ede1645f12126723c1202cb36552243717ef815e
lindsayRae <lbarnett712@gmail.com>
2018-06-26 00:14:26 -0400

    worked on charater sprites and user stats

--- app/js/components/nav-component.js
+++ app/js/components/nav-component.js
@@ -10,8 +10,8 @@ const navComponent = {
             console.log("yo");
             foodService.resetCount();
             foodService.resetBag();
-            angular.element(document.body).removeAttr("style");
-            angular.element(document.body).addClass("animate-background");
+            // angular.element(document.body).removeAttr("style");
+            // angular.element(document.body).addClass("animate-background");
             // angular.element(document.body).style.backgroundImage("url:'../img/linkhere.png'");
         }
     }]
